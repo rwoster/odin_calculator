314622a0258c1db745987b78a47e72e47dacaf15
Randy Woster <rwoster@protonmail.com>
2022-09-08 11:49:56 -0400

    Basic arithmetic functions

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const app = document.querySelector("#app");
+// const app = document.querySelector("#app");
 
-app.textContent = "hello, world";
+// app.textContent = "hello, world";
+
+// basic arithmetic functions
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
