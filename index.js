534c3b4b4e765e57161c70a62c68bed7542d15dc
Randy Woster <rwoster@protonmail.com>
2022-09-08 16:39:06 -0400

    Update CSS and HTML

--- index.js
+++ index.js
@@ -27,13 +27,49 @@ function operate(operator, operand1, operand2) {
 }
 
 const btnContainer = document.querySelector("#btn-container");
+const lowerDisplay = document.querySelector("#display-lower");
 
-const array = ["del", 0, "clear", 1, 2, 3, 4, 5, 6, 7, 8, 9];
-array.reverse();
+// const array = ["del", 0, "clear", 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const array = [
+    7,
+    8,
+    9,
+    "+",
+    4,
+    5,
+    6,
+    "-",
+    1,
+    2,
+    3,
+    "*",
+    ".",
+    0,
+    "=",
+    "/",
+    "clear",
+    "del",
+    "%",
+];
 
 array.forEach((item) => {
     const btn = document.createElement("div");
     btn.textContent = item;
+    if (item === "=") btn.classList.add("equals");
+    if (item === "del") btn.classList.add("del");
+    if (item === "clear") btn.classList.add("clear");
+    if (
+        item === "+" ||
+        item === "-" ||
+        item === "*" ||
+        item === "/" ||
+        item === "%"
+    )
+        btn.classList.add("operator");
     btn.classList.add("num");
     btnContainer.appendChild(btn);
+    btn.addEventListener("click", (e) => {
+        lowerDisplay.textContent = e.target.innerText;
+        console.log(typeof e.target.innerText);
+    });
 });
